e308ad7c3ed6d5971295bec3f2af3b5a9317d68b
Collin Lowing <collin.lowing@protonmail.com>
2022-11-01 14:55:58 -0500

    stubs for bin data structure

---
+++ Bin.hpp
@@ -0,0 +1,14 @@
+//
+// Created by cel on 11/1/22.
+//
+
+#ifndef COP4534_P4_BIN_HPP
+#define COP4534_P4_BIN_HPP
+
+
+class Bin {
+
+};
+
+
+#endif //COP4534_P4_BIN_HPP
---
+++ Item.hpp
@@ -0,0 +1,14 @@
+//
+// Created by cel on 11/1/22.
+//
+
+#ifndef COP4534_P4_ITEM_HPP
+#define COP4534_P4_ITEM_HPP
+
+
+class Item {
+
+};
+
+
+#endif //COP4534_P4_ITEM_HPP
